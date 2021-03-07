2a12c901215214deb5a1b2bcf40709a89db09ae9
yzhouas <zhouyuqian133@gmail.com>
2021-03-06 20:34:43 -0600

    rm not necessary

--- sgl_training/train.sh
+++
@@ -1,2 +0,0 @@
-python main.py --n_colors 1 --model UNET --loss 1*L1 --data_train DIV2K --data_test DIV2K --n_GPUs 1 --epochs 400 --data_range '1-750/1-50' --save Inpaint_0329 --scale 1 --patch_size 256 --reset --save_gt --save_results
-
